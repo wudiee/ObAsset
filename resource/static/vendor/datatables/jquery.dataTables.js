9a13f5a563b015dd12e311ad9cf2c0d974bde39c
ChoiJongBaek <59366717+ChoiJongBaek@users.noreply.github.com>
2021-02-01 19:30:34 +0900

    board commit

--- resource/static/vendor/datatables/jquery.dataTables.js
+++ resource/static/vendor/datatables/jquery.dataTables.js
@@ -11700,7 +11700,7 @@
 			 *      } );
 			 *    } );
 			 */
-			/*"sLengthMenu": "Show _MENU_ entries",*/
+			"sLengthMenu": "Show _MENU_ entries",
 	
 	
 			/**
--- src/com/ob/dao/BoardDAO.java
+++ src/com/ob/dao/BoardDAO.java
@@ -2,10 +2,15 @@ package com.ob.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ob.dto.Board;
 
+@Mapper
 public interface BoardDAO {
 	public List<Board> getBoards();
 	public Board getBoard(Board board);
 	public int addBoard(Board board);
+	public int updateBoard(Board board);
+	public int deleteBoard(Board board);
 }
